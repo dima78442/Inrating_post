8272e8114763f4998916ea0ec92d379c7eba2172
dima <dima7844@gmail.com>
2018-12-06 14:07:21 +0200

    init

---
+++ src/main/java/com/dima/inrating_post/myApplication.java
@@ -0,0 +1,11 @@
+package com.dima.inrating_post;
+
+import android.app.Application;
+
+public class myApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
